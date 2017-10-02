8c85307f36c8dd8a5456e8f9ec5d9c3da7f6a316
Suchismitha Vedala <suchi@Suchismithas-MacBook-Pro.local>
2017-10-02 17:42:15 -0500

    v3

--- region_analysis/binary_image.py
+++ region_analysis/binary_image.py
@@ -9,6 +9,10 @@ class binary_image:
         returns a histogram"""
 
         hist = [0]*256
+        for i in range(w):
+            for j in range(h):
+                hist[img[i,j]]=hist[img[i,j]]+1
+        
 
 
         return hist
@@ -20,9 +24,31 @@ class binary_image:
         returns: an optimal threshold value"""
 
         threshold = 0
+        t_in=int(255/2)
+        s=0
+        for i in range(t_in):
+            s=s+(i*hist[i])
+        p=0
+        for j in range(t_in):
+            p=p+hist[t_in]
+        e1=s/p
+        s1=0
+        for i in range(t_in,256):
+            s1=s1+(i*hist[i])
+        p1=0
+        for j in range(t_in,256):
+            p1=p1+hist[t_in]
+        e2=s1/p1
+        E1=[]
+        for i in range(t_in):
+            E1.append((i*hist[i])/e1)
+        E2=[]
+        for i in range(t_in,256):
+            E2.append((i*hist[i])/e2)
+        optimal=(np.float(sum(E1)) / max(len(E1), 1)  + np.float(sum(E2)) / max(len(E2), 1))/2
 
 
-        return threshold
+        return optimal
 
     def binarize(self, image):
         """Comptues the binary image of the the input image based on histogram analysis and thresholding
--- resize/resample.py
+++ resize/resample.py
@@ -22,10 +22,20 @@ class resample:
         fx: scale along y direction (eg. 0.5, 1.5, 2.5)
         returns a resized image based on the nearest neighbor interpolation method
         """
+        img = cv2.imread(image,0)
+        #plt.imshow(img)
+        #plt.show()
+        w,h,c = a.shape
+        new_w=int(w*fx)
+        new_h=int(h*fy)
+        b=np.zeros((new_h,new_w,c),np.uint8)
+        for i in range(new_w):
+            for j in range(new_h):
+                b[j,i,:]=img[int(i/fx)][int(j/fy)][:]
 
         #Write your code for nearest neighbor interpolation here
 
-        return image
+        return b
 
 
     def bilinear_interpolation(self, image, fx, fy):
@@ -37,6 +47,32 @@ class resample:
         """
 
         # Write your code for bilinear interpolation here
+        img = cv2.imread(image,1)
+        w,h,c = img.shape
+        new_w=int(w*fx)
+        new_h=int(h*fy)
+        b=np.zeros((new_h,new_w,c),np.uint8)
+        for i in range(new_w):
+            for j in range(new_h):
+                x1=int((np.floor(i/fx)))
+                x2=int((np.ceil(i/fx)))
+                if (x1 == 0):
+                    x1=1
+                x= 0
+                y1=int(np.floor(j/fy))
+                y2=int(np.ceil(j/fy))
+                if (y1 == 0):
+                    y1=1
+                y= 0
+                I1 = img[x1,y1,:]
+                I2 = img[x2,y1,:]       
+                I3 = img[x1,y2,:]
+                I4 = img[x2,y2,:]  
+                y = int((j/fx))
+                I5 = ((I3*y)+(I1*(1-y)))
+                I6 = ((I4*y)+(I2*(1-y))) 
+                b[j,i,:] = ((I6*x)+(I5*(1-x)))
+    
 
-        return image
+        return B
 
